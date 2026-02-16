5d5c302691c7553bac97228308e68b324c286cfe
rwilfong <rwilfong@purdue.edu>
2026-02-16 11:21:37 -0500

    windows script

--- recurring_transfer/monthly_sync.bat
+++ recurring_transfer/monthly_sync.bat
@@ -3,12 +3,15 @@
 REM Find conda 
 call "C:\ProgramData\anaconda3\Scripts\activate.bat"
 
-:: 2. Activate your specific environment
+REM Activate conda environment
 call conda activate your_env_name
 
-REM 
+REM Run the script 
 echo [%date% %time%] Starting Monthly Catchup in Conda environment... >> batch_log.txt
-python monthly_catchup.py --config config_nightly.ini >> batch_log.txt 2>&1
+python monthly_catchup.py --config config.ini --no-dry-run >> batch_log.txt 2>&1
 
 echo [%date% %time%] Process completed. >> batch_log.txt
-popd
+conda deactivate 
+
+REM Script for current month:
+python current_month_sync.py --config config.ini --no-dry-run >> batch_log.txt 2>&1
